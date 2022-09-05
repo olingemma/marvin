7e048a166462a76ddd3557ca731726629666cd37
Oling Emmanuel Benjamin <olingemmanuel5@gmail.com>
2022-09-05 19:44:46 +0300

    fixed the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Nav from "../components/nav";
 import styles from "../styles/Contact.module.css";
 import Head from "next/head";
+import Link from "next/link";
 
 
 export default function Contact(){
@@ -40,20 +41,21 @@ export default function Contact(){
                 <meta name="description" content="This is the contact me page for AM architects, kindly leave a message and we will get back to you as soon as possible."/>
             </Head>
             <div className={styles.main}>
-            <h1>Get in touch.</h1>
-            <form className={styles.form} method='post' onSubmit={handleSubmit}>
-               
-                <input className={styles.input} onChange={handleData} placeholder="Name" value={data.name} name="name"/>
-               
-                <input className={styles.input} onChange={handleData} placeholder="Email"value={data.email} name="email"/>
-               
-                <textarea className={styles.input} onChange={handleData} placeholder="Message" value={data.message} name="message"/>
-                <button>Message</button>
-            </form>
-
-            <p>{data.name}</p>
-            <p>{data.email}</p>
-            <p>{data.message}</p>
+            {/* <h1>Get in touch.</h1> */}
+                <form className={styles.form} method='post' onSubmit={handleSubmit}>
+                    <div className={styles.title}>Dear Asiimwe Marvin,</div>
+                    <textarea className={styles["input","message"]} onChange={handleData} placeholder="Write your message here..." value={data.message} name="message"/>
+                    <div className={styles.best}>Best,</div>
+                    <input className={styles["input","name"]} onChange={handleData} placeholder="Your name" value={data.name} name="Your name"/>
+                    <input className={styles["input","email"]} onChange={handleData} placeholder="Your email"value={data.email} name="Your email"/>
+                
+                    <button>Send email</button>
+                    <div className={styles.link}>
+                        <Link href="/">
+                            <p>or find me at <a style={{"textDecoration":"underline"}}>asiimwemarvin@gmail.com</a></p>
+                        </Link>
+                    </div>
+                </form>
             </div>
 
         </div>
